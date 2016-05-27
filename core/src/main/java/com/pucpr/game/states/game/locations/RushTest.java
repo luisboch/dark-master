5eae7a165912bf59b110591bc7cbaa570407f177
luisboch <luis.c.boch@gmail.com>
2016-05-27 16:17:08 -0300

    addded new level

---
+++ core/src/main/java/com/pucpr/game/states/game/locations/RushTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pucpr.game.states.game.locations;
+
+import com.pucpr.game.states.game.basic.BasicGameScreen;
+
+/**
+ *
+ * @author Luis Carlos
+ */
+public class RushTest extends BasicGameScreen{
+    
+    public RushTest() {
+        super("rushtestLevel.tmx");
+    }
+    
+}
